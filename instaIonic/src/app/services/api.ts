04e5242e092d13d32bdd8ec8910cd750ec103547
HYNR241 <hynr241@outlook.com>
2026-05-13 20:46:19 -0500

    Fix upload: replace manual authHeaders with HttpInterceptor to avoid Content-Type issues with FormData

--- instaIonic/src/app/services/api.ts
+++ instaIonic/src/app/services/api.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Auth } from './auth';
 
 
 @Injectable({
@@ -10,105 +9,96 @@ export class Api {
 
   private apiUrl = 'https://20.151.96.66/api/';
 
-  constructor(private http: HttpClient, private auth: Auth) {}
-
-  private authHeaders() {
-    const token = this.auth.getToken();
-    const headers: any = {};
-    if (token) headers['Authorization'] = `Bearer ${token}`;
-    return { headers };
-  }
+  constructor(private http: HttpClient) {}
 
   getMe() {
-    return this.http.get<any>(this.apiUrl + 'me', this.authHeaders());
+    return this.http.get<any>(this.apiUrl + 'me');
   }
 
   getFeed() {
-    return this.http.get<any>(this.apiUrl + 'posts', this.authHeaders());
+    return this.http.get<any>(this.apiUrl + 'posts');
   }
 
   getFriends() {
-    return this.http.get<any>(this.apiUrl + 'friends', this.authHeaders());
+    return this.http.get<any>(this.apiUrl + 'friends');
   }
 
   likePost(id: number) {
-    return this.http.post(this.apiUrl + `posts/${id}/like`, {}, this.authHeaders());
+    return this.http.post(this.apiUrl + `posts/${id}/like`, {});
   }
 
   createPost(file: File, caption: string) {
     const fd = new FormData();
     fd.append('image', file);
     fd.append('caption', caption);
-    return this.http.post(this.apiUrl + 'posts', fd, this.authHeaders());
+    return this.http.post(this.apiUrl + 'posts', fd);
   }
 
   commentPost(postId: number, content: string) {
     return this.http.post(
       this.apiUrl + `posts/${postId}/comments`,
-      { content },
-      this.authHeaders()
+      { content }
     );
   }
 
   getComments(postId: number) {
     return this.http.get<any[]>(
-      this.apiUrl + `posts/${postId}/comments`,
-      this.authHeaders()
+      this.apiUrl + `posts/${postId}/comments`
     );
   }
 
   searchUsers(q: string) {
-    return this.http.get<any[]>(this.apiUrl + `users/search?q=${q}`, this.authHeaders());
+    return this.http.get<any[]>(this.apiUrl + `users/search?q=${q}`);
   }
 
   sendFriendRequest(username: string) {
-    return this.http.post(this.apiUrl + `users/${username}/friend`, {}, this.authHeaders());
+    return this.http.post(this.apiUrl + `users/${username}/friend`, {});
   }
 
   getPendingFriendRequests() {
-    return this.http.get<any>(this.apiUrl + `friendships/pending`, this.authHeaders());
+    return this.http.get<any>(this.apiUrl + `friendships/pending`);
   }
 
   acceptFriendship(friendshipId: number) {
-    return this.http.post(this.apiUrl + `friendships/${friendshipId}/accept`, {}, this.authHeaders());
+    return this.http.post(this.apiUrl + `friendships/${friendshipId}/accept`, {});
   }
 
   getSuggestedUsers() {
-    return this.http.get<any[]>(this.apiUrl + 'users/suggested', this.authHeaders());
+    return this.http.get<any[]>(this.apiUrl + 'users/suggested');
   }
 
   removeFriend(username: string) {
-    return this.http.delete(this.apiUrl + `users/${username}/friend`, this.authHeaders());
+    return this.http.delete(this.apiUrl + `users/${username}/friend`);
   }
 
   getUserPosts(username: string) {
-    return this.http.get<any[]>(this.apiUrl + `users/${username}/posts`, this.authHeaders());
+    return this.http.get<any[]>(this.apiUrl + `users/${username}/posts`);
   }
 
   likeComment(commentId: number) {
-    return this.http.post(this.apiUrl + `comments/${commentId}/like`, {}, this.authHeaders());
+    return this.http.post(this.apiUrl + `comments/${commentId}/like`, {});
   }
 
   unlikeComment(commentId: number) {
-    return this.http.delete(this.apiUrl + `comments/${commentId}/like`, this.authHeaders());
+    return this.http.delete(this.apiUrl + `comments/${commentId}/like`);
   }
 
   cancelFriendRequest(username: string) {
-    return this.http.delete(this.apiUrl + `users/${username}/friend/cancel`, this.authHeaders());
+    return this.http.delete(this.apiUrl + `users/${username}/friend/cancel`);
   }
 
   getFriendshipStatus(username: string) {
-    return this.http.get(this.apiUrl + `users/${username}/friend/status`, this.authHeaders());
+    return this.http.get(this.apiUrl + `users/${username}/friend/status`);
   }
 
   updateProfile(data: { bio?: string; website?: string }) {
-    return this.http.put<any>(this.apiUrl + 'profile', data, this.authHeaders());
+    return this.http.put<any>(this.apiUrl + 'profile', data);
   }
 
   updateAvatar(file: File) {
     const fd = new FormData();
     fd.append('avatar', file);
-    return this.http.post<any>(this.apiUrl + 'profile/avatar', fd, this.authHeaders());
+    return this.http.post<any>(this.apiUrl + 'profile/avatar', fd);
   }
 
 }
